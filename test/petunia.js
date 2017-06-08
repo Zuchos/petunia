8b640424c8d3cfe0c0d3591e3b1948eb268d3a4b
zuchos <lukasz.zuchowski@gmail.com>
2017-06-08 19:33:53 +0200

    Improving code style in JS

--- test/petunia.js
+++ test/petunia.js
@@ -1,107 +1,105 @@
-var Petunia = artifacts.require("./Petunia.sol");
+var Petunia = artifacts.require('./Petunia.sol');
 
 function getBalance(account) {
-  console.log("checking balance for:" + account);
-  web3.eth.getBalance(account, function(e, r) {
-    console.log("Balance:")
+  return web3.eth.getBalance(account, (e, r) => {
     if (e)
-      console.log(e);
+      console.log('Error when checking balance:' + e);
     else
-      console.log(r.toNumber());
+      console.log('Balance:' + r.toNumber());
   });
 }
 
 var price = 1000000000;
 
-contract('Petunia', function(accounts) {
+contract('Petunia', (accounts) => {
   var petunia;
-  Petunia.deployed().then(function(instance) {
+  Petunia.deployed().then((instance) => {
     petunia = instance;
   });
 
-  it("should start new payment", function() {
-    const paymentId = "test1";
+  it('should start new payment', () => {
+    const paymentId = 'test1';
     const watcher = petunia.NewPayment();
     return petunia.startNewPayment(paymentId, price, {
       from: accounts[0]
-    }).then(function() {
+    }).then(() => {
       return watcher.get();
-    }).then(function(events) {
+    }).then((events) => {
       assert.equal(events.length, 1);
       actualPaymentId = events[0].args.externalPaymentId;
       actualPrice = events[0].args.price;
-      assert.equal(actualPrice,  price);
-      assert.equal(actualPaymentId,  paymentId);
+      assert.equal(actualPrice, price);
+      assert.equal(actualPaymentId, paymentId);
     });
   });
 
-  it("should check if payument exist", function() {
-    const paymentId = "test2";
-    return petunia.checkIfPaymentExists.call(paymentId).then(function(result) {
-      assert.equal(result,  false);
-    }).then(function() {
+  it('should check if payument exist', () => {
+    const paymentId = 'test2';
+    return petunia.checkIfPaymentExists.call(paymentId).then((result) => {
+      assert.equal(result, false);
+    }).then(() => {
       return petunia.startNewPayment(paymentId, price, {
         from: accounts[0]
       });
-    }).then(function() {
-      return petunia.checkIfPaymentExists.call(paymentId).then(function(result) {
-        assert.equal(result,  true);
+    }).then(() => {
+      return petunia.checkIfPaymentExists.call(paymentId).then((result) => {
+        assert.equal(result, true);
       });
     });
   });
 
-  it("payment creation should fail", function() {
-    const paymentId = "test3";
+  it('payment creation should fail', () => {
+    const paymentId = 'test3';
     return petunia.startNewPayment(paymentId, price, {
       from: accounts[0]
-    }).then(function() {
+    }).then(() => {
       return petunia.startNewPayment(paymentId, price, {
         from: accounts[0]
       });
-    }).then(function(events) {
+    }).then((events) => {
       assert.equal(false,  true);
-    }).catch(function(error) {
+    }).catch((error) => {
       assert.equal(true,  true);
     });
   });
 
-  it("should follow the payment process to the successful end", function() {
-    const paymentId = "test4";
+  it('should follow the payment process to the successful end', function() {
+    const paymentId = 'test4';
     var watcher = petunia.NewPayment();
     return petunia.startNewPayment(paymentId, price, {
       from: accounts[0]
-    }).then(function() {
+    }).then(() => {
       return watcher.get();
-    }).then(function(events) {
+    }).then((events) => {
       assert.equal(events[0].args.externalPaymentId, paymentId);
       return petunia.isPaid.call(paymentId);
-    }).then(function(bool) {
-      assert.equal(bool,  false, "Payment should be not paid yet");
-    }).then(function() {
+    }).then((bool) => {
+      assert.equal(bool, false, 'Payment should be not paid yet');
+    }).then(() => {
       watcher = petunia.PaymentSuccess();
       return petunia.pay(paymentId, {
         from: accounts[0],
         value: price
       });
-    }).then(function() {
+    }).then(() => {
       return watcher.get();
-    }).then(function(events) {
+    }).then((events) => {
       const paymentAccountBalance = events[0].args.paymentAccountBalance;
-      assert.equal(paymentAccountBalance,  price, "Payment account balance should be " + price);
+      assert.equal(paymentAccountBalance, price, 'Payment account balance should be ' + price);
       return petunia.isPaid.call(paymentId);
-    }).then(function(paid) {
-      assert.equal(paid,  true, "Payment should be paid.");
-    }).then(function() {
+    }).then((paid) => {
+      assert.equal(paid, true, 'Payment should be paid.');
+    }).then(() => {
       watcher = petunia.PaymentCompleted();
       return petunia.complete(paymentId, {
         from: accounts[0]
       });
-    }).then(function() {
+    }).then(() => {
       return watcher.get();
-    }).then(function(events) {
+    }).then((events) => {
       assert.equal(events.length, 1);
       const actualPaymentId = events[0].args.externalPaymentId;
-      assert.equal(actualPaymentId,  paymentId);
+      assert.equal(actualPaymentId, paymentId);
     });
   });
 });
