09f4a93edc4d18c7eaf3b1548d21063c0a4d4a31
zuchos <lukasz.zuchowski@gmail.com>
2017-09-20 14:33:20 +0200

    fakeshop removed

--- fakeshop/api/routes/fakeShopRoutes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function (app, http) {
-  var fakeShopController = require('../controllers/fakeShopController')(http);
-
-  // todoList Routes
-  app.route('/payment')
-    .post(fakeShopController.initPayment);
-};
